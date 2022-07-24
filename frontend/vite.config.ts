cbc482df9d62f7888ff222cb121e67e5b5a9aefa
Kawano Kenichi <kenichi.k.820@gmail.com>
2022-07-24 10:28:37 +0000

    :tada: First commit

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -8,6 +8,7 @@ import tsconfigPaths from "vite-tsconfig-paths";
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
   server: {
+    host: "0.0.0.0",
     port: 8080,
   },
   test: {
